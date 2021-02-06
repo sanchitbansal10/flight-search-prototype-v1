8cf1bc8d7d0a4d643a3390e44d4cc61db2184a08
sanchit bansal <sanchitbansal10@gmail.com>
2021-02-07 00:20:21 +0530

    feat(): add city and date select, disable search till all data present

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,9 @@
               :open-on-focus="true"
               field="PlaceName"
               @typing="onCityInput($event, 'from')"
+              @select="onCitySelect($event, 'from')"
             >
+              <template #empty>No results found</template>
             </b-autocomplete>
           </b-field>
         </div>
@@ -23,6 +25,7 @@
               :open-on-focus="true"
               field="PlaceName"
               @input="onCityInput($event, 'to')"
+              @select="onCitySelect($event,'to')"
             >
               <template #empty>No results found</template>
             </b-autocomplete>
@@ -36,13 +39,14 @@
               size="is-small"
               position="is-bottom-left"
               :min-date="new Date()"
+              @input="onDateInput"
             >
             </b-datepicker>
           </b-field>
         </div>
       </div>
       <div style="text-align:center">
-        <b-button type="is-info" size="is-small">
+        <b-button :disabled="allowFlightSearch" type="is-info" size="is-small">
           Search
         </b-button>
       </div>
@@ -51,32 +55,54 @@
 
 <script lang="ts">
 import store from "../store"
-import { IGetCityListResponse } from "@/helpers/types"
+import { ICityData, IGetCityListResponse } from "@/helpers/types";
 import debounce from 'lodash/debounce'
+import moment from "moment";
 
 export default {
-    name: "HelloWorld",
-    data() {
-      return {
-          fromCityList:[],
-          toCityList: []
+  name: "HelloWorld",
+  data() {
+    return {
+        fromCityList:[],
+        toCityList: [],
+        fromCity: null,
+        toCity: null,
+        departureDate: null
+    }
+  },
+  methods: {
+    onCityInput: debounce(function onCityInput(event: string, type: "from" | "to") {
+      if (event.length < 2) {
+        return
+      }
+      store.dispatch("fetchCities", event)
+              .then((data: IGetCityListResponse) => {
+                if (type === "from") {
+                  this.fromCityList = data.Places
+                } else {
+                  this.toCityList = data.Places
+                }
+              })
+    }, 500),
+
+    onCitySelect(data: ICityData, type: "from" | "to") {
+      if (type === "from") {
+        this.fromCity = data;
+      } else {
+        this.toCity = data
       }
     },
-    methods: {
-        onCityInput: debounce(function onCityInput (event: string,type: "from" | "to") {
-          if (event.length < 2) {
-            return
-          }
-          store.dispatch("fetchCities", event)
-            .then((data: IGetCityListResponse) => {
-                if (type === "from") {
-                    this.fromCityList = data.Places
-                } else {
-                    this.toCityList = data.Places
-                }
-            })
-        }, 500)
+
+    onDateInput(date: Date) {
+      this.departureDate = moment(date).format("YYYY-MM-DD");
+    },
+  },
+
+  computed: {
+    allowFlightSearch():boolean {
+      return !(this.fromCity && this.toCity && this.departureDate)
     }
+  }
 };
 </script>
 
